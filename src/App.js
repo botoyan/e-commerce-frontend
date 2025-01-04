d6d33bbecabba98c72d47ac9cda450de8b14d689
Artak Botoian <botoian@Artaks-MBP.lan>
2025-01-04 13:17:16 -0800

    ready for new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import HomePage from "./pages/HomePage";
-import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="sign-in" element={<SignIn />} />
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="/reset-password/:userId" element={<ResetPassword />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Hello from React</h1>
+    </div>
   );
 };
 
 export default App;
-
-/**
- * TODO need to add protected routes
- * TODO need to add popups instead of alerts to make it more beautiful
- */
